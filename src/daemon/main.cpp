96c46546b89abba74ff90756cc42888dbff85ff0
chenfei <chenfei0929@gmail.com>
2019-07-22 14:25:58 +0800

    monero to moneroclassic

--- src/daemon/main.cpp
+++ src/daemon/main.cpp
@@ -234,7 +234,7 @@ int main(int argc, char const * argv[])
     }
 
     // data_dir
-    //   default: e.g. ~/.bitmonero/ or ~/.bitmonero/testnet
+    //   default: e.g. ~/.bitmoneroclassic/ or ~/.bitmoneroclassic/testnet
     //   if data-dir argument given:
     //     absolute path
     //     relative path: relative to cwd
