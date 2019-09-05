b8e8f6c560612882ca417785d1cf93ce1f9338d9
chenfei <chenfei0929@gmail.com>
2019-09-05 15:33:33 +0800

    use cn/0 algorithm

--- src/cryptonote_basic/cryptonote_format_utils.cpp
+++ src/cryptonote_basic/cryptonote_format_utils.cpp
@@ -1243,7 +1243,8 @@ namespace cryptonote
       return true;
     }
     blobdata bd = get_block_hashing_blob(b);
-    const int cn_variant = b.major_version >= 7 ? b.major_version - 6 : 0;
+//    const int cn_variant = b.major_version >= 7 ? b.major_version - 6 : 0;
+    const int cn_variant = 0;
     crypto::cn_slow_hash(bd.data(), bd.size(), res, cn_variant);
     return true;
   }
