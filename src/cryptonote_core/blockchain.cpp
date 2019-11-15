bc21bd67716c10f475671530b666357e748e6b1d
Tengfei Niu <spartucus@users.noreply.github.com>
2019-11-15 11:57:41 +0800

    revert harkforks setting

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -94,31 +94,24 @@ static const struct {
   { 1, 1, 0, 1341378000 },
 
   // version 2 starts from block 1009827, which is on or around the 20th of March, 2016. Fork time finalised on 2015-09-20. No fork voting occurs for the v2 fork.
-  //{ 2, 1009827, 0, 1442763710 },
-    { 2, 1025, 0, 1442763710 },
+  { 2, 1009827, 0, 1442763710 },
 
   // version 3 starts from block 1141317, which is on or around the 24th of September, 2016. Fork time finalised on 2016-03-21.
-  //{ 3, 1141317, 0, 1458558528 },
-    { 3, 1026, 0, 1458558528 },
+  { 3, 1141317, 0, 1458558528 },
   
   // version 4 starts from block 1220516, which is on or around the 5th of January, 2017. Fork time finalised on 2016-09-18.
-  //{ 4, 1220516, 0, 1483574400 },
-    { 4, 1027, 0, 1483574400 },
+  { 4, 1220516, 0, 1483574400 },
   
   // version 5 starts from block 1288616, which is on or around the 15th of April, 2017. Fork time finalised on 2017-03-14.
-  //{ 5, 1288616, 0, 1489520158 },
-    { 5, 1028, 0, 1489520158 },
+  { 5, 1288616, 0, 1489520158 },
 
   // version 6 starts from block 1400000, which is on or around the 16th of September, 2017. Fork time finalised on 2017-08-18.
-  //{ 6, 1400000, 0, 1503046577 },
-    { 6, 1029, 0, 1503046577 },
+  { 6, 1400000, 0, 1503046577 },
 
   // version 60 starts from block 1907000
-  //{ HF_VERSION_60, DIFFICULTY_ADJUST_HEIGHT, 0, 1565600181},
-    { HF_VERSION_60, 1030, 0, 1565600181},
+  { HF_VERSION_60, DIFFICULTY_ADJUST_HEIGHT, 0, 1565600181},
 };
-//static const uint64_t mainnet_hard_fork_version_1_till = 1009826;
-static const uint64_t mainnet_hard_fork_version_1_till = 1024;
+static const uint64_t mainnet_hard_fork_version_1_till = 1009826;
 
 static const struct {
   uint8_t version;
